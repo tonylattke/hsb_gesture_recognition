6eca619221a6490b0dc033913a9974fe9a0f0a1c
fmindelis <fmindelis@stud.hs-bremen.de>
2017-01-20 14:58:28 +0100

    - added not empty check

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -77,48 +77,53 @@ def analyze(ip):
     if thresh1 is not None:
         # Contours
         contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        cnt = 0
         try:
+            global cnt
             cnt = max(contours, key=lambda x: cv2.contourArea(x))
         except (ValueError, TypeError):
             print('empty list or invalid input')
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(crop_img, (x, y), (x + w, y + h), ip.blue, ip.lineThickness)
-        drawing = np.zeros(crop_img.shape, np.uint8)
-        cv2.drawContours(drawing, [cnt], 0, ip.green, ip.lineThickness)
+            cnt = 0
 
-        # Convex Hull
-        hull = cv2.convexHull(cnt)
-        cv2.drawContours(drawing, [hull], 0, ip.red, ip.lineThickness)
+        if cnt is not 0:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(crop_img, (x, y), (x + w, y + h), ip.blue, ip.lineThickness)
+            drawing = np.zeros(crop_img.shape, np.uint8)
+            cv2.drawContours(drawing, [cnt], 0, ip.green, ip.lineThickness)
 
-        # Calculate Defect points
-        hullWithoutPoints = cv2.convexHull(cnt, returnPoints=False)
-        defects = cv2.convexityDefects(cnt, hullWithoutPoints)
-        count_defects = 0
-        triangles = []
-        if defects is not None:
-            for i in range(defects.shape[0]):
-                s, e, f, d = defects[i, 0]
-                start = tuple(cnt[s][0])
-                end = tuple(cnt[e][0])
-                far = tuple(cnt[f][0])
-                angle = gh.calculateAngle(start,end,far)
-                if angle <= 90:
-                    count_defects += 1
-                    triangle = th.Triangle(start,end,far)
-                    triangles.append(triangle)
-                cv2.line(crop_img, start, end, ip.red, ip.lineThickness)
+            # Convex Hull
+            hull = cv2.convexHull(cnt)
+            cv2.drawContours(drawing, [hull], 0, ip.red, ip.lineThickness)
 
-        # Calculate center of hand
-        centerOfHand = th.listToTuple(th.obtainCenterOfHand(triangles))
+            # Calculate Defect points
+            hullWithoutPoints = cv2.convexHull(cnt, returnPoints=False)
+            defects = cv2.convexityDefects(cnt, hullWithoutPoints)
+            count_defects = 0
+            triangles = []
+            if defects is not None:
+                for i in range(defects.shape[0]):
+                    s, e, f, d = defects[i, 0]
+                    start = tuple(cnt[s][0])
+                    end = tuple(cnt[e][0])
+                    far = tuple(cnt[f][0])
+                    angle = gh.calculateAngle(start,end,far)
+                    if angle <= 90:
+                        count_defects += 1
+                        triangle = th.Triangle(start,end,far)
+                        triangles.append(triangle)
+                    cv2.line(crop_img, start, end, ip.red, ip.lineThickness)
 
-        handModel = hfm.FilipsModel(centerOfHand,triangles)
-        ip.numberOfFingers = handModel.countOfFinger()
-        handModel.getCenter()
-        handModel.drawLines(drawing, ip.magenta, ip.lineThickness)
-        handModel.drawDefects(drawing, ip.yellow, ip.defectInternRadius, ip.defectExternRadius)
-        handModel2 = htm.TonyModel(handModel.center, triangles)
-        handModel2.drawLines(drawing, ip.cyan, ip.lineThickness)
+            # Calculate center of hand
+            centerOfHand = th.listToTuple(th.obtainCenterOfHand(triangles))
 
-        # cv2.imshow('Contours', all_img)
-        cv2.imwrite("drawing.png", drawing)
-        cv2.imwrite("crop_img.png", crop_img)
+            handModel = hfm.FilipsModel(centerOfHand,triangles)
+            ip.numberOfFingers = handModel.countOfFinger()
+            handModel.getCenter()
+            handModel.drawLines(drawing, ip.magenta, ip.lineThickness)
+            handModel.drawDefects(drawing, ip.yellow, ip.defectInternRadius, ip.defectExternRadius)
+            handModel2 = htm.TonyModel(handModel.center, triangles)
+            handModel2.drawLines(drawing, ip.cyan, ip.lineThickness)
+
+            # cv2.imshow('Contours', all_img)
+            cv2.imwrite("drawing.png", drawing)
+            cv2.imwrite("crop_img.png", crop_img)
